f2f20a1ee210bae0f24600d37f52e8fa88ba6340
Grace Kendall <gkendall@apple.com>
2014-06-16 10:55:18 -0700

    Initial Commit

---
+++ CItest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CItest
+//
+//  Created by Grace Kendall on 6/16/14.
+//  Copyright (c) 2014 Grace Kendall. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
